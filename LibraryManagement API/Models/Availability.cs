1cb57a434cec87e095decc7db02e1f354d2767c8
Akhil Goud Burra <124227421+Akhil-Goud-Burra@users.noreply.github.com>
2024-11-11 17:43:56 -0600

    DatabaseDesign and Scaffolding

---
+++ LibraryManagement API/Models/Availability.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryManagement_API.Models;
+
+[Table("Availability")]
+public partial class Availability
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Unicode(false)]
+    public string Quantity { get; set; } = null!;
+
+    public bool Status { get; set; }
+
+    public int BookId { get; set; }
+
+    [ForeignKey("BookId")]
+    [InverseProperty("Availabilities")]
+    public virtual Book Book { get; set; } = null!;
+}
---
+++ LibraryManagement API/Models/Stream.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryManagement_API.Models;
+
+[Table("STREAM")]
+public partial class Stream
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Unicode(false)]
+    public string Name { get; set; } = null!;
+
+    [InverseProperty("Stream")]
+    public virtual ICollection<Book> Books { get; set; } = new List<Book>();
+}
