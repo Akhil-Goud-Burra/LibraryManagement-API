54b6db9bd183d90bcd77f95ceffcf1d20b9d1731
Akhil Goud Burra <124227421+Akhil-Goud-Burra@users.noreply.github.com>
2024-11-14 12:03:34 -0600

    Repository Pattern Implementation

---
+++ LibraryManagement API/RepositoryPattern/IRepositoryImplementation/IRepositoryStreamImplementation.cs
@@ -0,0 +1,16 @@
+﻿using LibraryManagement_API.Models;
+using LibraryManagement_API.RepositoryPattern.IRepository;
+using System;
+
+namespace LibraryManagement_API.RepositoryPattern.IRepositoryImplementation
+{
+    public class IRepositoryStreamImplementation : IRepositoryStream
+    {
+        private readonly MyDbContext _appDbContext;
+
+        public IRepositoryStreamImplementation(MyDbContext appDbContext) 
+        { 
+            _appDbContext = appDbContext;
+        }
+    }
+}
