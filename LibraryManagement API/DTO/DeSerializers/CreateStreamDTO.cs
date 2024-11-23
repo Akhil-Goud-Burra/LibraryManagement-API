383133147cad6f71097fa840591c76a025e64e6f
Akhil Goud Burra <124227421+Akhil-Goud-Burra@users.noreply.github.com>
2024-11-22 22:31:46 -0600

    Custom Data Validation in Stream

--- LibraryManagement API/DTO/DeSerializers/CreateStreamDTO.cs
+++ LibraryManagement API/DTO/DeSerializers/CreateStreamDTO.cs
@@ -1,12 +1,14 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using LibraryManagement_API.Custom_Validators.Stream_Validators;
+using System.ComponentModel.DataAnnotations;
 
 namespace LibraryManagement_API.DTO.DeSerializers
 {
     public class CreateStreamDTO
     {
-        [Required]
+        [Required(ErrorMessage = "Enter the Stream Name")]
         [MinLength(1)]
         [MaxLength(10)]
+        [StreamNameValidator(ErrorMessage = "values not specified in AllowedValuesAttribute")]
         public string Name { get; set; } = null!;
     }
 }
