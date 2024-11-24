556acb74e50636545c105ab313c458b2f4ccc677
Akhil Goud Burra <124227421+Akhil-Goud-Burra@users.noreply.github.com>
2024-11-23 20:58:26 -0600

    Exception Filter Setup for sending proper error responses

---
+++ LibraryManagement API/Error Handling/Custom Exception Setup/CustomApiException.cs
@@ -0,0 +1,12 @@
+﻿namespace LibraryManagement_API.Error_Handling.Custom_Exception_Setup
+{
+    public class CustomApiException : Exception
+    {
+        public int StatusCode { get; set; }
+        public CustomApiException(string message, int statusCode = 500) : base(message)
+        {
+            StatusCode = statusCode;
+        }
+
+    }
+}
