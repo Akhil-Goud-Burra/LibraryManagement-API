34c033581c7d759ce04d47e2ba5b31285ad4bc4a
Akhil Goud Burra <124227421+Akhil-Goud-Burra@users.noreply.github.com>
2024-11-30 23:00:33 -0600

    Update Stream WIth Delete Method

---
+++ LibraryManagement API/DTO/DeSerializers/Stream/DeleteStreamDTO.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibraryManagement_API.DTO.DeSerializers.Stream
+{
+    public class DeleteStreamDTO
+    {
+        [Required(ErrorMessage = "Enter the Stream Id")]
+        [Range(0, 100)]
+        public int Id { get; set; }
+    }
+}
